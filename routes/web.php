c658035c6f5d4b2383eccdee7bbef6c2ba3fbe2d
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-12-03 09:46:51 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(ThemeController::class)->group(function () {
+    Route::get('/', 'index')->name('home.index');
+    Route::get('/about', 'about')->name('home.about');
 });
