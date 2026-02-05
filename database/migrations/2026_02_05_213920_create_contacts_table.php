abff811be2a05ab0a6867b2b19a8b248340418f2
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2026-02-05 23:46:28 +0200

    Update Table

--- database/migrations/2026_02_05_213920_create_contacts_table.php
+++ database/migrations/2026_02_05_213920_create_contacts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name')->nullable;
+            $table->string('last_name')->nullable;
+            $table->string('email')->nullable;
+            $table->text('message')->nullable;
             $table->timestamps();
         });
     }
